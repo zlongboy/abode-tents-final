0ab7cbce035eabc4abee7b94b61f727662405774
zlongboy <zlongboy@me.com>
2022-09-06 18:40:47 -0400

    link repo

---
+++ application/app.js
@@ -0,0 +1,27 @@
+require('Dotenv').config();
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const homeRoute = require('./routes/home');
+const shopRoute = require('./routes/shop');
+const aboutRoute = require('./routes/about');
+const pdpRoute = require('./routes/pdp');
+const compareRoute = require('./routes/compare');
+
+app.use('/', homeRoute);
+app.use('/shop', shopRoute);
+app.use('/contact', aboutRoute);
+app.use('/kelty-rumpus', pdpRoute);
+app.use('/compare', compareRoute);
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
